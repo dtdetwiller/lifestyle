160320c050b06494221a32cbd86fa5c187f88d7e
jakewhiteyo <jakewhiteyo@gmail.com>
2022-02-21 20:42:34 -0700

    added profile picture

--- app/src/main/java/com/example/lifestyle/HomeFragment.java
+++ app/src/main/java/com/example/lifestyle/HomeFragment.java
@@ -1,18 +1,25 @@
 package com.example.lifestyle;
 
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.ImageView;
+
+import java.io.File;
 
 public class HomeFragment extends Fragment {
 
 
     private Button mapsHikeButton;
+    private ImageView imageView;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -25,6 +32,21 @@ public class HomeFragment extends Fragment {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_home, container, false);
 
+    }
 
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        imageView = (ImageView) view.findViewById(R.id.profile_image);
+        File imageFile = new File(getActivity().getFilesDir(), "ProfileImage.png");
+
+        if (imageFile.exists()) {
+            try {
+                imageView.setImageDrawable(Drawable.createFromPath(imageFile.toString()));
+
+            } catch (Exception e) {
+
+            }
+        }
     }
 }
