5ea58edcc2563aedbc4cbea6f3985fa9bb879d46
jakewhiteyo <jakewhiteyo@gmail.com>
2022-02-22 18:51:49 -0700

    edited profile page

--- app/src/main/java/com/example/lifestyle/ProfilePageFragment.java
+++ app/src/main/java/com/example/lifestyle/ProfilePageFragment.java
@@ -1,21 +1,42 @@
 package com.example.lifestyle;
 
+import android.content.Intent;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
+import android.provider.MediaStore;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link ProfilePageFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+import java.io.File;
+import java.util.Scanner;
+
+
 public class ProfilePageFragment extends Fragment {
+
+    private ImageView profile_picture;
+    private TextView name_view;
+    private TextView body_view;
+    private Button edit_profile;
+
+    private String first_name;
+    private String last_name;
+    private String gender;
+    private String height_feet;
+    private String height_inches;
+    private String weight;
+    private String city;
+    private String country;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -32,5 +53,76 @@ public class ProfilePageFragment extends Fragment {
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
+        profile_picture = (ImageView) view.findViewById(R.id.profile_image);
+        File imageFile = new File(getActivity().getFilesDir(), "ProfileImage.png");
+
+        if (imageFile.exists()) {
+            try {
+                profile_picture.setImageDrawable(Drawable.createFromPath(imageFile.toString()));
+
+            } catch (Exception e) {
+
+            }
+        }
+
+        readFile();
+
+        name_view = (TextView) view.findViewById(R.id.name_view);
+        if(first_name != "") {
+            name_view.setText(first_name + " " + last_name);
+        }
+        body_view = (TextView) view.findViewById(R.id.body_view);
+        if(height_feet != "") {
+            body_view.setText(height_feet + "'" + height_inches + " ");
+        }
+
+        edit_profile = (Button) view.findViewById(R.id.edit_profile);
+        edit_profile.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+    }
+
+    private void readFile() {
+        File nameFile = new File(getActivity().getFilesDir(), "Profile");
+
+        if(nameFile.exists()) {
+            try {
+                Scanner scanner = new Scanner(nameFile);
+                int i = 0;
+                while (scanner.hasNextLine()) {
+                    String line = scanner.nextLine();
+                    String[] words = line.split(" ");
+                    if(words[0].equals("first_name"))
+                        first_name = words[1];
+                    else if(words[0].equals("last_name"))
+                        last_name = words[1];
+                    else if(words[0].equals("gender"))
+                        gender = words[1];
+                    else if(words[0].equals("height_feet"))
+                        height_feet = words[1];
+                    else if(words[0].equals("height_inches"))
+                        height_inches = words[1];
+                    else if(words[0].equals("weight"))
+                        weight = words[1];
+                    else if(words[0].equals("city")) {
+                        city = "";
+                        for (int j = 1; j < words.length; j++)
+                            city = city + " " + words[j];
+                    }
+                    else if(words[0].equals("country")) {
+                        country = "";
+                        for (int j = 1; j < words.length; j++)
+                            country = country + " " + words[j];
+                    }
+                    i++;
+                }
+            } catch (Exception e) {
+
+            }
+        }
     }
 }
