1330072d38b2ff47c43486f8e44bb8e0a9e45e12
jakewhiteyo <jakewhiteyo@gmail.com>
2022-02-22 17:55:06 -0700

    created profile page

---
+++ app/src/main/java/com/example/lifestyle/ProfilePageFragment.java
@@ -0,0 +1,36 @@
+package com.example.lifestyle;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link ProfilePageFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class ProfilePageFragment extends Fragment {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_profile_page, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+    }
+}
