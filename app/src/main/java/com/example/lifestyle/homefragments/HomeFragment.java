2d5b8406e11896a61dc545ce2ae0673a0fadd9fd
DANIEL DETWILLER <dtdetwiller@gmail.com>
2022-02-28 11:57:25 -0700

    BMR and calories to eat now displaying on homepage.

--- app/src/main/java/com/example/lifestyle/homefragments/HomeFragment.java
+++ app/src/main/java/com/example/lifestyle/homefragments/HomeFragment.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.annotation.RequiresPermission;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
@@ -30,16 +31,14 @@ public class HomeFragment extends Fragment {
     private String first_name = "";
     private String last_name = "";
     private String activityLevel = "";
-    private String caloriestoEat = "";
-    private String weightChange = "";
+    private int caloriesToEat;
+    private String weightGoal = "";
     private String gender = "";
     private String weight = "";
-    private String height_feet = "";
-    private String height_inches = "";
+    private String heightFeet = "";
+    private String heightInches = "";
     private String age = "";
-
-    private File profileFile;
-    private File fitnessGoals;
+    private String poundsPerWeek = "";
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -57,9 +56,9 @@ public class HomeFragment extends Fragment {
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-        profileFile = new File(getActivity().getFilesDir(), "Profile");
-        fitnessGoals = new File(getActivity().getFilesDir(), "FitnessGoals");
-        readFilesNecessary();
+
+        ReadFile();
+
         imageView = (ImageView) view.findViewById(R.id.profile_image);
         File imageFile = new File(getActivity().getFilesDir(), "ProfileImage.png");
 
@@ -75,72 +74,87 @@ public class HomeFragment extends Fragment {
         welcomeText = getView().findViewById(R.id.welcome_text);
         calorieField = getView().findViewById(R.id.caloriesToEatToLoseorGainWeight);
         welcomeText.setText("Welcome " + first_name + " " + last_name);
-        displayGoalsandCaloriestoEat();
-        }
 
-
-    public void displayGoalsandCaloriestoEat() {
-        caloriestoEat = "1000";
-        double BMR = calculateBMR();
-        calorieField.setText("Your BMR is: " + BMR +" According to your fitness goals you want to " + weightChange +"." +
-                "To do this you must eat " + caloriestoEat + " a week.");
-
+        DisplayGoals();
     }
 
-    public double calculateBMR()
+
+    public void DisplayGoals() {
+
+        caloriesToEat = 0;
+        double BMR = CalculateBMR();
+        int pounds = Integer.valueOf(poundsPerWeek);
+
+        if (activityLevel.equals("Sedentary")) {
+            caloriesToEat = (int)Math.round(BMR * 1.2);
+        }
+        else if (activityLevel.equals("Active")) {
+            caloriesToEat = (int)Math.round(BMR * 1.55);
+        }
+
+        if (weightGoal.equals("Lose")) {
+            int weeklyCalories = caloriesToEat * 7;
+            int calorieDeficit = pounds * 3500;
+
+            weeklyCalories = weeklyCalories - calorieDeficit;
+            caloriesToEat = weeklyCalories / 7;
+        }
+
+        if (weightGoal.equals("Gain")) {
+            int weeklyCalories = caloriesToEat * 7;
+            int calorieSurplus = pounds * 3500;
+
+            weeklyCalories = weeklyCalories + calorieSurplus;
+            caloriesToEat = weeklyCalories / 7;
+        }
+
+        calorieField.setText("Your BMR is: " + BMR +" According to your fitness goals you want to " + weightGoal +"." +
+                "To do this you must eat " + caloriesToEat + " a week.");
+    }
+
+    public double CalculateBMR()
     {
-        double bmr = 0;
-        double totalHeight = Integer.valueOf(height_feet)*12 + Integer.valueOf(height_inches);
-        //male = 88.362 + (13.397 x weight in kg) + (4.799 x height in cm) – (5.677 x age in years)
-        //female = 447.593 + (9.247 x weight in kg) + (3.098 x height in cm) – (4.330 x age in years)
-        if(gender.equals("male"))
-            bmr = 88.362 + (13.397 * (Integer.valueOf(weight)/2.205)) + (4.799 * (totalHeight*2.54)) - (5.677 *Integer.valueOf(age));
-        else if(gender.equals("female"))
-            bmr = 447.593 + (9.247 * (Integer.valueOf(weight)/2.205)) + (3.098 * (totalHeight*2.54)) - (4.330 *Integer.valueOf(age));
+        double bmr = 0.0;
+        int totalInches = (Integer.valueOf(heightFeet) * 12) + Integer.valueOf(heightInches);
+        int lbs = Integer.valueOf(weight);
+
+        if(gender.equals("Male"))
+            bmr = 66 + (6.3 * lbs) + (12.9 * totalInches) - (6.8 * Integer.valueOf(age));
+        else if(gender.equals("Female"))
+            bmr = 655 + (4.3 * lbs) + (4.7 * totalInches) - (4.7 * Integer.valueOf(age));
         return bmr;
     }
 
-    public void readFilesNecessary() {
-        if (profileFile.exists()) {
+    /**
+     * Reads the FitnessGoals file and sets all the values.
+     */
+    private void ReadFile() {
+
+        File nameFile = new File(getActivity().getFilesDir(), "FitnessGoals");
+
+        if(nameFile.exists()) {
             try {
-                Scanner scnr = new Scanner(profileFile);
-
-                while (scnr.hasNextLine()) {
-                    String line = scnr.nextLine();
-                    String[] tokens = line.split(" ");
-
-                    if (tokens[0].equals("first_name"))
-                        first_name = tokens[1];
-                    else if (tokens[0].equals("last_name"))
-                        last_name = tokens[1];
-                    else if(tokens[0].equals("gender"))
-                        gender = tokens[1];
-                    else if(tokens[0].equals("weight"))
-                        weight = tokens[1];
-                    else if(tokens[0].equals("height_feet"))
-                        height_feet = tokens[1];
-                    else if(tokens[0].equals("height_inches"))
-                        height_inches = tokens[1];
-
-                }
-            } catch (Exception e) {
-                System.out.println("There was an error trying to read the Profile file.");
-            }
-        }
-
-        if (fitnessGoals.exists()) {
-            try {
-                Scanner scanner = new Scanner(fitnessGoals);
+                Scanner scanner = new Scanner(nameFile);
                 int i = 0;
                 while (scanner.hasNextLine()) {
                     String line = scanner.nextLine();
                     String[] words = line.split(" ");
-                    if (words[0].equals("activityLevel"))
-                        activityLevel = words[1];
-                    else if (words[0].equals("weightGoal"))
-                        weightChange = words[1];
+                    if(words[0].equals("gender"))
+                        gender = words[1];
+                    else if(words[0].equals("heightFeet"))
+                        heightFeet = words[1];
+                    else if(words[0].equals("heightInches"))
+                        heightInches = words[1];
+                    else if(words[0].equals("weight"))
+                        weight = words[1];
                     else if(words[0].equals("age"))
                         age = words[1];
+                    else if(words[0].equals("activityLevel"))
+                        activityLevel = words[1];
+                    else if(words[0].equals("poundsPerWeek"))
+                        poundsPerWeek = words[1];
+                    else if(words[0].equals("weightGoal"))
+                        weightGoal = words[1];
                 }
             } catch (Exception e) {
 
