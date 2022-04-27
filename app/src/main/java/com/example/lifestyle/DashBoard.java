f7e2873f9ada4dc3edfd0e588ad4b2a5079cde13
Justin Perez <u1248103@umail.utah.edu>
2022-04-26 19:24:24 -0600

    s

--- app/src/main/java/com/example/lifestyle/DashBoard.java
+++ app/src/main/java/com/example/lifestyle/DashBoard.java
@@ -16,7 +16,7 @@ import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.navigation.NavigationBarView;
 
 public class DashBoard extends AppCompatActivity {
-
+    //stuff
     public WeatherViewModel weatherViewModel;
 
     @Override
